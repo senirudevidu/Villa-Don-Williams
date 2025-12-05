e510db79761b97786812112b911a831fab399b18
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-12-05 09:51:16 +0530

    Navbar

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import "./Button.css";
+function Button({ className, text }) {
+  return <button className={className}>{text}</button>;
+}
+
+export default Button;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./Navbar.css";
+import Button from "../Button/Button.jsx";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          Villa Don Williams
-        </Link>
+      <div className="logo">
+        <img src="./logo3.png" alt="logo" />
+      </div>
+      <div className="nav-items">
+        <a href="#rooms">Rooms</a>
+        <a href="#gallery">Gallery</a>
+        <a href="#reviews">Reviews</a>
+        <a href="#contact">Contact Us</a>
+      </div>
+      <div className="btns">
+        <Button className="booking-com-btn" text="Booking.com" />
+        <Button className="airbnb-btn" text="Airbnb" />
       </div>
     </nav>
   );
