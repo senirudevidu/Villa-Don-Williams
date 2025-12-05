6f50fce7bc022ab6d3461d8050788f2edce17791
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-12-05 14:32:05 +0530

    photo gallery

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import Location from "./Components/Location/Location.jsx";
 import Reviews from "./Components/Reviews/Reviews.jsx";
+import VisualTour from "./Components/VisualTour/VisualTour.jsx";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Location />
       <Reviews />
+      <VisualTour />
     </>
   );
 }
---
+++ src/Components/VisualTour/VisualTour.jsx
@@ -0,0 +1,25 @@
+import "./VisualTour.css";
+
+const images = import.meta.glob(
+  "/src/assets/PhotoGallery/*.{jpg,jpeg,png,webp,JPEG}",
+  { eager: true }
+);
+
+const allImages = Object.values(images).map((img) => img.default);
+
+function VisualTour() {
+  return (
+    <section className="visual-tour-section">
+      <h2 className="visual-tour-title">Visual Tour</h2>
+      <p className="visual-tour-desc">See the place as guesrs experience it</p>
+      <div className="gallery">
+        {allImages.map((src, index) => (
+          <div className="img">
+            <img key={index} src={src} alt={`gallery-${index}`} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+export default VisualTour;
